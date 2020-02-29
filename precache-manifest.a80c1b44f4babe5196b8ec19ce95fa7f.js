3d63352e779b650c8f69ae798adb99d7569cbf91
Chiranjeeb Sahoo <CSahoo@ESTUATE.COM>
2020-02-29 18:37:03 +0530

    Updates

---
+++ precache-manifest.a80c1b44f4babe5196b8ec19ce95fa7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa8cb8b5690071c15b99924ae919802",
+    "url": "/migartion_hub/index.html"
+  },
+  {
+    "revision": "bbb73f8d327d4442dd44",
+    "url": "/migartion_hub/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "6bd1bd2fe4ac5ce5c5db",
+    "url": "/migartion_hub/static/js/2.bcba5007.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/migartion_hub/static/js/2.bcba5007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb73f8d327d4442dd44",
+    "url": "/migartion_hub/static/js/main.ff2d4901.chunk.js"
+  },
+  {
+    "revision": "ca8e7eb3e3ec21725fe1",
+    "url": "/migartion_hub/static/js/runtime-main.9a926119.js"
+  },
+  {
+    "revision": "19811fde9f2551b65e2f524f037a1210",
+    "url": "/migartion_hub/static/media/Login_Banner.19811fde.png"
+  },
+  {
+    "revision": "e91c00334b06c083131e743bb9698944",
+    "url": "/migartion_hub/static/media/Migration_Hub_logo.e91c0033.png"
+  }
+]);
